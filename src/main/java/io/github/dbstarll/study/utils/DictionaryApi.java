ceef6ea2beae0a7cd83e8f344d30ce2ae3dcde50
dbstarll <dbstarll@gmail.com>
2023-03-02 22:34:36 +0800

    使用EnumValueHelper来解析值

--- src/main/java/io/github/dbstarll/study/utils/DictionaryApi.java
+++ src/main/java/io/github/dbstarll/study/utils/DictionaryApi.java
@@ -3,6 +3,7 @@ package io.github.dbstarll.study.utils;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.dbstarll.dubai.model.entity.EntityFactory;
+import io.github.dbstarll.dubai.model.entity.utils.EnumValueHelper;
 import io.github.dbstarll.study.classify.exchange.ExchangeUtils;
 import io.github.dbstarll.study.entity.Word;
 import io.github.dbstarll.study.entity.enums.ExchangeKey;
@@ -28,129 +29,132 @@ import static org.apache.commons.lang3.Validate.notBlank;
 import static org.apache.commons.lang3.Validate.notNull;
 
 public class DictionaryApi {
-  private static final String BASE_URL = "http://dict-co.iciba.com/api/dictionary.php?type=json";
+    private static final String BASE_URL = "http://dict-co.iciba.com/api/dictionary.php?type=json";
 
-  private final String queryUrl;
-  private final ObjectMapper objectMapper;
+    private static final EnumValueHelper<ExchangeKey> EXCHANGE_KEY_HELPER = new EnumValueHelper<>(ExchangeKey.class);
+    private static final EnumValueHelper<PartKey> PART_KEY_HELPER = new EnumValueHelper<>(PartKey.class);
 
-  public DictionaryApi(String key, ObjectMapper objectMapper) {
-    this.queryUrl = BASE_URL + "&key=" + notBlank(key);
-    this.objectMapper = notNull(objectMapper);
-  }
+    private final String queryUrl;
+    private final ObjectMapper objectMapper;
 
-  /**
-   * 查询网络词典并组装成Word对象.
-   *
-   * @param word 待查的单词
-   * @return 单词对应的Word对象，如果没有匹配的释义，则返回null
-   * @throws IOException 网络异常
-   */
-  public Word query(String word) throws IOException {
-    if (StringUtils.containsWhitespace(notBlank(word))) {
-      throw new IllegalArgumentException("word contains whitespace: " + word);
+    public DictionaryApi(String key, ObjectMapper objectMapper) {
+        this.queryUrl = BASE_URL + "&key=" + notBlank(key);
+        this.objectMapper = notNull(objectMapper);
     }
-    final URL url = new URL(queryUrl + "&w=" + word.toLowerCase());
-    final String json = IOUtils.toString(url, StandardCharsets.UTF_8);
-    return parseWord(objectMapper.readTree(json));
-  }
 
-  private static Word parseWord(final JsonNode node) throws IOException {
-    final JsonNode cri = node.get("is_CRI");
-    if (cri != null) {
-      final Word word = EntityFactory.newInstance(Word.class);
-      word.setCri(cri.asInt() == 1);
-      word.setName(node.get("word_name").asText().trim());
-      word.setExchanges(parseExchanges(word.getName(), node.get("exchange")));
-      final JsonNode symbols = node.get("symbols");
-      if (symbols != null && symbols.isArray() && symbols.size() > 0) {
-        final JsonNode symbol = symbols.get(0);
-        word.setPhonetics(parsePhonetics(symbol));
-        word.setParts(parseParts(symbol.get("parts")));
-      }
-      return word;
-    }
-    return null;
-  }
-
-  private static Set<Exchange> parseExchanges(final String word, final JsonNode node) {
-    final Set<Exchange> exchanges = new HashSet<>();
-    if (node != null && !node.isNull()) {
-      for (Map.Entry<String, JsonNode> entry : IterableWrapper.wrap(node.fields())) {
-        final String key = entry.getKey();
-        final JsonNode exchangeNode = entry.getValue();
-        if (key.startsWith("word_") && exchangeNode.isArray() && exchangeNode.size() > 0) {
-          final ExchangeKey exchangeKey;
-          try {
-            exchangeKey = ExchangeKey.valueOf(key.substring(5));
-          } catch (IllegalArgumentException ex) {
-            throw new IllegalArgumentException("Unknown ExchangeKey:" + key, ex);
-          }
-
-          final String exchangeValue = exchangeNode.get(0).asText().trim();
-          exchanges
-                  .add(new Exchange(exchangeKey, exchangeValue, ExchangeUtils.classify(exchangeKey, word, exchangeValue)));
+    /**
+     * 查询网络词典并组装成Word对象.
+     *
+     * @param word 待查的单词
+     * @return 单词对应的Word对象，如果没有匹配的释义，则返回null
+     * @throws IOException 网络异常
+     */
+    public Word query(String word) throws IOException {
+        if (StringUtils.containsWhitespace(notBlank(word))) {
+            throw new IllegalArgumentException("word contains whitespace: " + word);
         }
-      }
+        final URL url = new URL(queryUrl + "&w=" + word.toLowerCase());
+        final String json = IOUtils.toString(url, StandardCharsets.UTF_8);
+        return parseWord(objectMapper.readTree(json));
     }
-    return exchanges.isEmpty() ? null : exchanges;
-  }
 
-  private static Set<Phonetic> parsePhonetics(final JsonNode node) throws IOException {
-    final Set<Phonetic> phonetics = new HashSet<>();
-    if (node != null && !node.isNull()) {
-      for (PhoneticKey key : PhoneticKey.values()) {
-        final JsonNode symbolNode = node.get("ph_" + key.name());
-        if (symbolNode != null && !symbolNode.isNull()) {
-          final String symbol = symbolNode.asText().trim();
-          if (StringUtils.isNotBlank(symbol)) {
-            final Phonetic phonetic = new Phonetic(key, symbol);
-            final JsonNode urlNode = node.get("ph_" + key.name() + "_mp3");
-            if (urlNode != null && !urlNode.isNull()) {
-              final String url = urlNode.asText();
-              if (StringUtils.isNotBlank(url)) {
-                phonetic.mp3(IOUtils.toByteArray(new URL(url)));
-              }
+    private static Word parseWord(final JsonNode node) throws IOException {
+        final JsonNode cri = node.get("is_CRI");
+        if (cri != null) {
+            final Word word = EntityFactory.newInstance(Word.class);
+            word.setCri(cri.asInt() == 1);
+            word.setName(node.get("word_name").asText().trim());
+            word.setExchanges(parseExchanges(word.getName(), node.get("exchange")));
+            final JsonNode symbols = node.get("symbols");
+            if (symbols != null && symbols.isArray() && symbols.size() > 0) {
+                final JsonNode symbol = symbols.get(0);
+                word.setPhonetics(parsePhonetics(symbol));
+                word.setParts(parseParts(symbol.get("parts")));
             }
-            phonetics.add(phonetic);
-          }
+            return word;
         }
-      }
+        return null;
     }
-    return phonetics.isEmpty() ? null : phonetics;
-  }
 
-  private static Set<Part> parseParts(final JsonNode node) {
-    final Set<Part> parts = new HashSet<>();
-    if (node != null && node.isArray()) {
-      for (JsonNode partNode : node) {
-        final String key = partNode.get("part").asText();
-        final JsonNode meansNode = partNode.get("means");
-        if (key.endsWith(".") && meansNode != null && meansNode.isArray() && meansNode.size() > 0) {
-          final List<PartKey> partKey = parsePartKey(key);
-          final List<String> means = new ArrayList<>(meansNode.size());
-          for (JsonNode mean : meansNode) {
-            means.add(mean.asText().trim());
-          }
-          parts.add(new Part(partKey, means));
-        }
-      }
-    }
-    return parts.isEmpty() ? null : parts;
-  }
+    private static Set<Exchange> parseExchanges(final String word, final JsonNode node) {
+        final Set<Exchange> exchanges = new HashSet<>();
+        if (node != null && !node.isNull()) {
+            for (Map.Entry<String, JsonNode> entry : IterableWrapper.wrap(node.fields())) {
+                final String key = entry.getKey();
+                final JsonNode exchangeNode = entry.getValue();
+                if (key.startsWith("word_") && exchangeNode.isArray() && exchangeNode.size() > 0) {
+                    final ExchangeKey exchangeKey;
+                    try {
+                        exchangeKey = EXCHANGE_KEY_HELPER.valueOf(key.substring(5));
+                    } catch (IllegalArgumentException ex) {
+                        throw new IllegalArgumentException("Unknown ExchangeKey:" + key, ex);
+                    }
 
-  private static List<PartKey> parsePartKey(final String key) {
-    final List<PartKey> keys = new ArrayList<>();
-    for (String k : StringUtils.split(key.replaceAll("\\.| ", "").replace('-', '_'), '&')) {
-      try {
-        keys.add(PartKey.valueOf(k));
-      } catch (IllegalArgumentException ex) {
-        try {
-          keys.add(PartKey.valueOf('_' + k));
-        } catch (IllegalArgumentException ex2) {
-          throw new IllegalArgumentException("Unknown PartKey:" + key, ex);
+                    final String exchangeValue = exchangeNode.get(0).asText().trim();
+                    exchanges
+                            .add(new Exchange(exchangeKey, exchangeValue, ExchangeUtils.classify(exchangeKey, word, exchangeValue)));
+                }
+            }
         }
-      }
+        return exchanges.isEmpty() ? null : exchanges;
     }
-    return keys;
-  }
+
+    private static Set<Phonetic> parsePhonetics(final JsonNode node) throws IOException {
+        final Set<Phonetic> phonetics = new HashSet<>();
+        if (node != null && !node.isNull()) {
+            for (PhoneticKey key : PhoneticKey.values()) {
+                final JsonNode symbolNode = node.get("ph_" + key.name());
+                if (symbolNode != null && !symbolNode.isNull()) {
+                    final String symbol = symbolNode.asText().trim();
+                    if (StringUtils.isNotBlank(symbol)) {
+                        final Phonetic phonetic = new Phonetic(key, symbol);
+                        final JsonNode urlNode = node.get("ph_" + key.name() + "_mp3");
+                        if (urlNode != null && !urlNode.isNull()) {
+                            final String url = urlNode.asText();
+                            if (StringUtils.isNotBlank(url)) {
+                                phonetic.mp3(IOUtils.toByteArray(new URL(url)));
+                            }
+                        }
+                        phonetics.add(phonetic);
+                    }
+                }
+            }
+        }
+        return phonetics.isEmpty() ? null : phonetics;
+    }
+
+    private static Set<Part> parseParts(final JsonNode node) {
+        final Set<Part> parts = new HashSet<>();
+        if (node != null && node.isArray()) {
+            for (JsonNode partNode : node) {
+                final String key = partNode.get("part").asText();
+                final JsonNode meansNode = partNode.get("means");
+                if (key.endsWith(".") && meansNode != null && meansNode.isArray() && meansNode.size() > 0) {
+                    final List<PartKey> partKey = parsePartKey(key);
+                    final List<String> means = new ArrayList<>(meansNode.size());
+                    for (JsonNode mean : meansNode) {
+                        means.add(mean.asText().trim());
+                    }
+                    parts.add(new Part(partKey, means));
+                }
+            }
+        }
+        return parts.isEmpty() ? null : parts;
+    }
+
+    private static List<PartKey> parsePartKey(final String key) {
+        final List<PartKey> keys = new ArrayList<>();
+        for (String k : StringUtils.split(key.replaceAll("\\.| ", "").replace('-', '_'), '&')) {
+            try {
+                keys.add(PART_KEY_HELPER.valueOf(k));
+            } catch (IllegalArgumentException ex) {
+                try {
+                    keys.add(PART_KEY_HELPER.valueOf('_' + k));
+                } catch (IllegalArgumentException ex2) {
+                    throw new IllegalArgumentException("Unknown PartKey:" + key, ex);
+                }
+            }
+        }
+        return keys;
+    }
 }

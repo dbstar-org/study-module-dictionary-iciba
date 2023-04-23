5ee6e75592be0d7094e6007ab38ad3ebd186461b
dbstarll <dbstarll@gmail.com>
2023-04-23 15:51:34 +0800

    迁移Principal、PrincipalService

--- src/main/java/io/github/dbstarll/study/entity/enums/Page.java
+++ src/main/java/io/github/dbstarll/study/entity/enums/Page.java
@@ -1,50 +1,52 @@
 package io.github.dbstarll.study.entity.enums;
 
-import io.github.dbstarll.study.entity.Principal;
-
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public enum Page {
-  // ENGLISH
-  // word(Module.ENGLISH),
+    // ENGLISH
+    // word(Module.ENGLISH),
 
-  book(Module.ENGLISH),
+    book(Module.ENGLISH),
 
-  exercise_book(Module.ENGLISH, Principal.Mode.USER),
+    exercise_book(Module.ENGLISH, Mode.USER),
 
-  spell(Module.ENGLISH, Principal.Mode.USER, Principal.Mode.GUEST),
+    spell(Module.ENGLISH, Mode.USER, Mode.GUEST),
 
-  spell_exchange(Module.ENGLISH, Principal.Mode.USER),
+    spell_exchange(Module.ENGLISH, Mode.USER),
 
-  // listen(Module.ENGLISH, Mode.USER),
+    // listen(Module.ENGLISH, Mode.USER),
 
-  // read(Module.ENGLISH, Mode.USER),
+    // read(Module.ENGLISH, Mode.USER),
 
-  // MATH
-  // custom(Module.MATH, Mode.USER),
+    // MATH
+    // custom(Module.MATH, Mode.USER),
 
-  // mix(Module.MATH, Mode.USER, Mode.GUEST),
+    // mix(Module.MATH, Mode.USER, Mode.GUEST),
 
-  // SETTING
+    // SETTING
 
-  user(Module.SETTING);
+    user(Module.SETTING);
 
-  // approve(Module.SETTING);
+    // approve(Module.SETTING);
 
-  public final Module module;
+    public final Module module;
 
-  private final Set<Principal.Mode> modes;
+    private final Set<Mode> modes;
 
-  private Page(final Module module, final Principal.Mode... modes) {
-    this.module = module;
-    this.modes = new HashSet<>(Arrays.asList(modes));
-  }
+    private Page(final Module module, final Mode... modes) {
+        this.module = module;
+        this.modes = new HashSet<>(Arrays.asList(modes));
+    }
 
-  public Collection<Principal.Mode> modes() {
-    return Collections.unmodifiableCollection(modes);
-  }
+    public Collection<Mode> modes() {
+        return Collections.unmodifiableCollection(modes);
+    }
 
-  public boolean mode(Principal.Mode mode) {
-    return modes.contains(mode);
-  }
+    public boolean mode(Mode mode) {
+        return modes.contains(mode);
+    }
 }
